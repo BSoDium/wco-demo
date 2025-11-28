034c3ad75d0e48754fa0b4ca992bf6a858f7b408
Elliot Négrel-Jerzy <negreljerzy.elliot@gmail.com>
2025-11-28 01:40:27 +0100

    refactor: clean up useThemeColor hook and remove unneeded cleanup logic

--- src/hooks/useThemeColor.ts
+++ src/hooks/useThemeColor.ts
@@ -4,7 +4,8 @@ import { useEffect } from "react";
 export default function useThemeColor() {
   const theme = useTheme();
   const surface = theme.palette.background.surface;
-  const themeColor = surface.match(/#([0-9a-fA-F]{6}|[0-9a-fA-F]{3})/)?.[0] || "#ffffff";
+  const themeColor =
+    surface.match(/#([0-9a-fA-F]{6}|[0-9a-fA-F]{3})/)?.[0] || "#ffffff";
 
   useEffect(() => {
     // Set theme-color meta tag
@@ -15,12 +16,5 @@ export default function useThemeColor() {
       document.head.appendChild(themeColorMetaTag);
     }
     themeColorMetaTag.setAttribute("content", themeColor);
-
-    return () => {
-      // Clean up on unmount
-      if (themeColorMetaTag) {
-        document.head.removeChild(themeColorMetaTag);
-      }
-    };
   }, [themeColor]);
 }
